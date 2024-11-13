0783182d3998404c1197d9648524aa2f38ed0c70
Ricardo Akkari <softwareengricky@outlook.com>
2024-11-13 16:14:10 +1100

    added react query, users hook and entity

---
+++ Contacts/src/hooks/useUsers.ts
@@ -0,0 +1,12 @@
+import User from "@/entities/User";
+import APIClient from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+const apiClient = new APIClient<User>("/users");
+
+const useUsers = useQuery({
+  queryKey: ["users"],
+  queryFn: apiClient.getAll,
+});
+
+export default useUsers;
