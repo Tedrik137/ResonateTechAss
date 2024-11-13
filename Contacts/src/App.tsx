06cd0e3518cd2f2f2c6a5cc3a295346462c729da
Ricardo Akkari <softwareengricky@outlook.com>
2024-11-13 12:58:52 +1100

    configured chakra-ui

--- Contacts/src/App.tsx
+++ Contacts/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button, HStack } from "@chakra-ui/react";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <HStack>
+      <Button>Click Me</Button>
+      <Button>Click Me</Button>
+    </HStack>
+  );
+};
 
-export default App
+export default App;
